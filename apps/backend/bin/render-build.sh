91dfc4a7bcaea19b7c98b397d359d23cc04220a9
Christian Najera <chris.najera.zambrano@gmail.com>
2024-07-13 13:23:50 -0600

    playwright

--- apps/backend/bin/render-build.sh
+++ apps/backend/bin/render-build.sh
@@ -11,11 +11,13 @@ npx nx run backend:install
 # Generate Prisma client
 npx nx run backend:prisma
 
-# Store/pull Prisma cache with build cache
-if [[ ! -d $PRISMA_BINARY_CACHE_DIR ]]; then
-    echo "...Copying Prisma Binary Cache from Build Cache"
-    cp -R $XDG_CACHE_HOME/prisma/binaries $PRISMA_BINARY_CACHE_DIR
-else
-    echo "...Storing Prisma Binary Cache in Build Cache"
-    cp -R $PRISMA_BINARY_CACHE_DIR $XDG_CACHE_HOME
-fi
+playwright install
+
+# # Store/pull Prisma cache with build cache
+# if [[ ! -d $PRISMA_BINARY_CACHE_DIR ]]; then
+#     echo "...Copying Prisma Binary Cache from Build Cache"
+#     cp -R $XDG_CACHE_HOME/prisma/binaries $PRISMA_BINARY_CACHE_DIR
+# else
+#     echo "...Storing Prisma Binary Cache in Build Cache"
+#     cp -R $PRISMA_BINARY_CACHE_DIR $XDG_CACHE_HOME
+# fi
